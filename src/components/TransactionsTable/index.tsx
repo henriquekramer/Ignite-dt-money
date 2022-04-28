a6dc286cbae69afd948d1addeba13d2c2ae4e96e
Henrique <henriquemkramer@gmail.com>
2022-04-28 18:13:49 -0300

    first commit

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,39 @@
+import { useEffect } from "react";
+import { api } from "../../services/api";
+import { Container } from "./styles";
+
+export function TransactionsTable(){
+  useEffect(() => {
+    api.get('transactions').then(response => console.log(response.data))
+  }, []);
+
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Preço</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de website</td>
+            <td className="deposit">R$ 12.000</td>
+            <td>Desenvolvimento</td>
+            <td>13/04/201</td>
+          </tr>
+          <tr>
+            <td>Aluguel</td>
+            <td className="withdraw">- R$ 1.100</td>
+            <td>Casa</td>
+            <td>17/04/201</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  )
+}
