7fcdc51fb90598c7f53e0d7cc3a91c86f8742650
Henrique <henriquemkramer@gmail.com>
2022-04-29 16:18:13 -0300

    finalização projeto

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import { useState } from 'react';
 import Modal from 'react-modal'
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from './components/NewTransactionModal';
 
 import { GlobalStyle } from "./styles/global";
+import { TransactionsProvider } from './hooks/useTransactions';
 
 Modal.setAppElement('#root');  // de acordo com a documentação do react-modal é necessário para acessibilidade
 
 export function App() {
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
 
   function handleOpenNewTransactionModal(){
     setIsNewTransactionModalOpen(true);
@@ -20,20 +23,19 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
 
       <Dashboard />
 
-      <Modal 
-          isOpen={isNewTransactionModalOpen}
-          onRequestClose={handleCloseNewTransactionModal}
-        >
-          <h2>Cadastrar transação</h2>
-      </Modal>
-
+      <NewTransactionModal 
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
 
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
+
+
